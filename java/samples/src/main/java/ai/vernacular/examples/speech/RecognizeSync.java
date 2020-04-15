3e87df11d43600099ef9978a0bf8c0ae2ec3342e
Deepankar Agrawal <deepankar11093@gmail.com>
2020-04-16 03:50:48 +0530

    fix proto compile errors

--- java/samples/src/main/java/ai/vernacular/examples/speech/RecognizeSync.java
+++ java/samples/src/main/java/ai/vernacular/examples/speech/RecognizeSync.java
@@ -1,6 +1,13 @@
 package ai.vernacular.examples.speech;
 
+import ai.vernacular.speech.RecognitionAudio;
+// import ai.vernacular.speech.RecognitionConfig;
+// import ai.vernacular.speech.RecognizeRequest;
+// import ai.vernacular.speech.RecognizeResponse;
 import ai.vernacular.speech.SpeechClient;
+// import ai.vernacular.speech.SpeechRecognitionAlternative;
+// import ai.vernacular.speech.SpeechRecognitionResult;
+// import com.google.protobuf.ByteString;
 
 public class RecognizeSync {
 
--- java/vernacular-ai-speech/src/main/java/ai/vernacular/speech/SpeechClient.java
+++ java/vernacular-ai-speech/src/main/java/ai/vernacular/speech/SpeechClient.java
@@ -1,5 +1,9 @@
 package ai.vernacular.speech;
 
+// import com.google.api.gax.core.BackgroundResource;
+
+import ai.vernacular.speech.RecognitionAudio;
+
 public class SpeechClient {
     
 }

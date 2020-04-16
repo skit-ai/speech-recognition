146e3a34921b8b832e9f2da45a2256e2aa4e24d4
Deepankar Agrawal <deepankar11093@gmail.com>
2020-04-16 20:21:06 +0530

    add recognize method

--- java/samples/src/main/java/ai/vernacular/examples/speech/RecognizeSync.java
+++ java/samples/src/main/java/ai/vernacular/examples/speech/RecognizeSync.java
@@ -1,18 +1,49 @@
 package ai.vernacular.examples.speech;
 
-import ai.vernacular.speech.RecognitionAudio;
-// import ai.vernacular.speech.RecognitionConfig;
-// import ai.vernacular.speech.RecognizeRequest;
-// import ai.vernacular.speech.RecognizeResponse;
 import ai.vernacular.speech.SpeechClient;
-// import ai.vernacular.speech.SpeechRecognitionAlternative;
-// import ai.vernacular.speech.SpeechRecognitionResult;
-// import com.google.protobuf.ByteString;
+import ai.vernacular.speech.RecognitionAudio;
+import ai.vernacular.speech.RecognitionConfig;
+import ai.vernacular.speech.SpeechRecognitionAlternative;
+import ai.vernacular.speech.RecognitionConfig.AudioEncoding;
+import ai.vernacular.speech.RecognizeResponse;
+import ai.vernacular.speech.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RecognizeSync {
 
-    public static void main(String[] args) {
-        SpeechClient speechClient = new SpeechClient();
+    public static void sampleRecognize(String accessToken, String localFilePath) {
+        try (SpeechClient speechClient = new SpeechClient(accessToken)) {
+            // The language of the supplied audio
+            String languageCode = "en-IN";
 
+            // Sample rate in Hertz of the audio data sent
+            int sampleRateHertz = 8000;
+
+            // Encoding of audio data sent. This sample sets this explicitly.
+            RecognitionConfig.AudioEncoding encoding = RecognitionConfig.AudioEncoding.LINEAR16;
+            RecognitionConfig config = RecognitionConfig.newBuilder().setLanguageCode(languageCode)
+                    .setSampleRateHertz(sampleRateHertz).setEncoding(encoding).build();
+            Path path = Paths.get(localFilePath);
+            byte[] data = Files.readAllBytes(path);
+            ByteString content = ByteString.copyFrom(data);
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(content).build();
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            for (SpeechRecognitionResult result : response.getResultsList()) {
+                // First alternative is the most probable result
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcript: %s\n", alternative.getTranscript());
+            }
+        } catch (IOException exception) {
+            System.err.println("Failed to create the client due to: " + exception.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        sampleRecognize("vernacularai", "hello.wav");
     }
 }
--- java/vernacular-ai-speech/src/main/java/ai/vernacular/speech/SpeechClient.java
+++ java/vernacular-ai-speech/src/main/java/ai/vernacular/speech/SpeechClient.java
@@ -1,9 +1,44 @@
 package ai.vernacular.speech;
 
-// import com.google.api.gax.core.BackgroundResource;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Metadata;
+import ai.vernacular.speech.SpeechToTextGrpc;
+
+import java.io.IOException;
 
 import ai.vernacular.speech.RecognitionAudio;
+import ai.vernacular.speech.RecognizeRequest;
+import ai.vernacular.speech.RecognizeResponse;
+import ai.vernacular.speech.RecognitionAudio;
+import ai.vernacular.speech.RecognitionConfig;
 
-public class SpeechClient {
-    
+public class SpeechClient implements AutoCloseable{
+
+    public static final String STTP_GRPC_HOST = "localhost";
+    public static final int STTP_GRPC_PORT = 5021;
+    public static final String AUTHORIZATION = "authorization";
+
+    private String accessToken;
+    private ManagedChannel channel;
+    private SpeechToTextGrpc.SpeechToTextBlockingStub channelStub;
+
+    public SpeechClient(String accessToken) {
+        this.accessToken = accessToken;
+
+        this.channel = ManagedChannelBuilder.forAddress(STTP_GRPC_HOST, STTP_GRPC_PORT).usePlaintext().build();
+        this.channelStub = SpeechToTextGrpc.newBlockingStub(channel);
+    }
+
+    public final RecognizeResponse recognize(RecognitionConfig config, RecognitionAudio audio) {
+        // return recognizeCallable().call(request);
+        RecognizeRequest request = RecognizeRequest.newBuilder().setConfig(config).setAudio(audio).build();
+        return this.channelStub.recognize(request);
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+
 }

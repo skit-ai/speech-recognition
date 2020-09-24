e38908dd97f27d3a62627883312df1c2f1d4b059
Deepankar Agrawal <deepankar11093@gmail.com>
2020-09-25 03:07:09 +0530

    update streaming example with multithreading

--- python/samples/recognize_streaming.py
+++ python/samples/recognize_streaming.py
@@ -1,6 +1,9 @@
 from vernacular.ai import speech
 from vernacular.ai.speech import enums, types
 import os
+import time
+import threading
+from six.moves import queue
 
 
 def infer_encoding(file_path: str):
@@ -14,36 +17,67 @@ def infer_encoding(file_path: str):
     return enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED
 
 
-def sample_recognize_streaming(access_token, file_path):
-    speech_client = speech.SpeechClient(access_token)
+class AddAudio(threading.Thread):
+   def __init__(self, file_path, _buff):
+      threading.Thread.__init__(self)
+      self.file_path = file_path
+      self._buff = _buff
 
-    with open(file_path, "rb") as file:
+   def run(self):
+      with open(self.file_path, "rb") as file:
         audio_content = file.read()
 
-    config = types.RecognitionConfig(
-        encoding=infer_encoding(file_path),
-        sample_rate_hertz=8000,
-        language_code="en-IN",
-        max_alternatives=1,
-    )
-    stream_config = types.StreamingRecognitionConfig(
-        config=config,
-    )
+        for i in range(0, len(audio_content), 8000):
+            self._buff.put(audio_content[i:i+8000])
+            # add a delay for real time streaming simulation
+            time.sleep(0.1)
 
-    # this should ideally be a generator yielding chunks of audio data
-    requests = []
-    for i in range(0, len(audio_content), 10000):
-        requests.append(
-            types.StreamingRecognizeRequest(audio_content=audio_content[i:i+10000])
+        # add None to queue to mark end of streaming
+        self._buff.put(None)
+
+
+class SampleRecognizeStreaming():
+    def __init__(self, access_token, file_path):
+        self.speech_client = speech.SpeechClient(access_token)
+        self.file_path = file_path
+
+        config = types.RecognitionConfig(
+            encoding=infer_encoding(file_path),
+            sample_rate_hertz=8000,
+            language_code="en-IN",
+            max_alternatives=1,
+        )
+        self.stream_config = types.StreamingRecognitionConfig(
+            config=config,
         )
 
-    responses = speech_client.streaming_recognize(stream_config, requests)
-    for response in responses:
-        for result in response.results:
-            # First alternative is the most probable result
-            alternative = result.alternatives[0]
-            print("Transcript: {}".format(alternative.transcript))
-            print("Confidence: {}".format(alternative.confidence))
+        self._buff = queue.Queue()
+
+    def run(self):
+        requests = (types.StreamingRecognizeRequest(audio_content=content)
+                    for content in self.audio_generator())
+
+        responses = self.speech_client.streaming_recognize(self.stream_config, requests)
+
+        # add audios in a new thread to simulate streaming
+        t1 = AddAudio(self.file_path, self._buff)
+        t1.start()
+
+        # this is blocking call and will wait until server sends the result
+        for response in responses:
+            for result in response.results:
+                alternative = result.alternatives[0]
+                print("Transcript: {}".format(alternative.transcript))
+                print("Confidence: {}".format(alternative.confidence))
+
+
+    def audio_generator(self):
+        while True:
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+
+            yield chunk
 
 
 def main():
@@ -58,7 +92,8 @@ def main():
     )
     args = parser.parse_args()
 
-    sample_recognize_streaming(args.access_token, args.file_path)
+    ss = SampleRecognizeStreaming(args.access_token, args.file_path)
+    ss.run()
 
 
 if __name__ == "__main__":
